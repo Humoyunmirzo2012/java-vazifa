155cbe86c6ec0a63dd978e77f83ff926d9a94285
Humoyunmirzo2012 <webdevoper0509@gmail.com>
2024-08-02 19:29:31 +0500

    java-vazifa

---
+++ js/app.js
@@ -0,0 +1,66 @@
+//     // // // // // // // // // Stringlar ustida amallarni bajarish
+//     // // // // // // // // let firstName = "Ali";
+//     // // // // // // // // let lastName = "Valiyev";
+//     // // // // // // // // let fullName = firstName + " " + lastName;
+
+//     // // // // // // // // console.log("To'liq ism: " + fullName); // "To'liq ism: Ali Valiyev"
+
+//     // // // // // // // // // String uzunligini topish
+//     // // // // // // // // console.log("Ism uzunligi: " + firstName.length); // "Ism uzunligi: 3"
+
+//     // // // // // // // // Raqamlar ustida amallarni bajarish
+//     // // // // // // // let x = 10;
+//     // // // // // // // let y = 5;
+
+//     // // // // // // // let sum = x + y;
+//     // // // // // // // let difference = x - y;
+//     // // // // // // // let product = x * y;
+//     // // // // // // // let quotient = x / y;
+
+//     // // // // // // // console.log("Yig'indi: " + sum); // "Yig'indi: 15"
+//     // // // // // // // console.log("Farq: " + difference); // "Farq: 5"
+//     // // // // // // // console.log("Ko'paytma: " + product); // "Ko'paytma: 50"
+//     // // // // // // // console.log("Bo'linma: " + quotient); // "Bo'linma: 2"
+
+//     // // // // // // // Boolean qiymatlar
+//     // // // // // // let isRaining = true;
+//     // // // // // // let isSunny = false;
+
+//     // // // // // // console.log("Yomg'ir yog'ayaptimi? " + isRaining); // "Yomg'ir yog'ayaptimi? true"
+//     // // // // // // console.log("Quyosh chiqayaptimi? " + isSunny); // "Quyosh chiqayaptimi? false"
+
+//     // // // // // // Massivlar yaratish va ularda amallar bajarish
+//     // // // // // let fruits = ["Olma", "Banan", "Shaftoli"];
+//     // // // // // console.log("Mevalar: " + fruits); // "Mevalar: Olma,Banan,Shaftoli"
+
+//     // // // // // // Massivga yangi element qo'shish
+//     // // // // // fruits.push("Uzum");
+//     // // // // // console.log("Yangi mevalar: " + fruits); // "Yangi mevalar: Olma,Banan,Shaftoli,Uzum"
+
+//     // // // // // // Massivning uzunligini topish
+//     // // // // // console.log("Mevalar soni: " + fruits.length); // "Mevalar soni: 4"
+
+//     // // // // // Ob'ekt yaratish va ulardan foydalanish
+//     // // // // let person = {
+//     // // // //     name: "Ali",
+//     // // // //     age: 25,
+//     // // // //     isStudent: true
+//     // // // // };
+
+//     // // // // console.log("Ism: " + person.name); // "Ism: Ali"
+//     // // // // console.log("Yosh: " + person.age); // "Yosh: 25"
+//     // // // // console.log("Talaba: " + person.isStudent); // "Talaba: true"
+
+//     // // // // // Ob'ektga yangi xususiyat qo'shish
+//     // // // // person.city = "Toshkent";
+//     // // // // console.log("Shahar: " + person.city); // "Shahar: Toshkent"
+
+//     // // // let isStudent = true;
+//     // // // let hasGraduated = false;
+
+//     // // let emptyValue = null;
+
+//     // let notAssigned;
+
+//     let uniqueID = Symbol("id");
+// let bigNumber = 1234567890123456789012345678901234567890n;
